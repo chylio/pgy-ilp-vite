64d0b6796d13fd0a7e60ee752c02e52dfe6e04ea
Chylio <chylio127@gmail.com>
2025-08-12 00:00:20 +0800

    sync before pull

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/pgy-ilp-vite/',   // ← 你的 repo 名稱
-})
 
+
+export default defineConfig({
+  base: '/pgy-ilp-vite/',
+  plugins: [react()],
+})
 
